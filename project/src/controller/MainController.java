433693b4ebd8052e00cfa87298494bd3dc18ea36
nhidh99 <nhidh99@gmail.com>
2019-10-07 05:10:25 +0700

    Nghiệp vụ: Xóa loại phòng
    Nghiệp vụ xóa loại phòng: chỉ xóa khi không còn phòng nào thuộc loại phòng cần xóa

--- project/src/controller/MainController.java
+++ project/src/controller/MainController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import BUS.*;
@@ -9,60 +10,102 @@ import DTO.*;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MainController implements Initializable {
-	@FXML TableView <NhanVienDTO> tvNhanVien;
-	@FXML TableColumn<NhanVienDTO, Integer> tcNV_STT;
-	@FXML TableColumn<NhanVienDTO, Integer> tcNV_MaNhanVien;
-	@FXML TableColumn<NhanVienDTO, String> tcNV_TenNhanVien;
-	@FXML TableColumn<NhanVienDTO, String> tcNV_CMND;
-	@FXML TableColumn<NhanVienDTO, String> tcNV_SoDienThoai;
-	@FXML TableColumn<NhanVienDTO, String> tcNV_DiaChi;
-	@FXML TableColumn<NhanVienDTO, String> tcNV_Email;
-	@FXML TableColumn<NhanVienDTO, String> tcNV_ChucVu;
-	
-	@FXML Label lbTS_SoNgayTraCoc;
-	@FXML Label lbTS_TiLeThueVAT;
-	@FXML Label lbTS_TiLeTienCoc;
-	@FXML Label lbTS_QuaKhach;
-	@FXML Label lbTS_TraPhongTre;
-	
-	@FXML TableView <LoaiDichVuDTO> tvLoaiDichVu;
-	@FXML TableColumn<LoaiDichVuDTO, Integer> tcLDV_STT;
-	@FXML TableColumn<LoaiDichVuDTO, Integer> tcLDV_MaLoaiDV;
-	@FXML TableColumn<LoaiDichVuDTO, String> tcLDV_TenLoaiDV;
-	
-	@FXML TableView <NhaCungCapDTO> tvNhaCungCap;
-	@FXML TableColumn<NhaCungCapDTO, Integer> tcNCC_STT;
-	@FXML TableColumn<NhaCungCapDTO, Integer> tcNCC_MaNhaCC;
-	@FXML TableColumn<NhaCungCapDTO, String> tcNCC_TenNhaCC;
-	@FXML TableColumn<NhaCungCapDTO, Integer> tcNCC_SoDienThoai;
-	
-	@FXML TableView <LoaiPhongDTO> tvLoaiPhong;
-	@FXML TableColumn<LoaiPhongDTO, Integer> tcLP_STT;
-	@FXML TableColumn<LoaiPhongDTO, Integer> tcLP_MaLoaiPhong;
-	@FXML TableColumn<LoaiPhongDTO, String> tcLP_TenLoaiPhong;
-	@FXML TableColumn<LoaiPhongDTO, Integer> tcLP_SoKhachToiDa;
-	@FXML TableColumn<LoaiPhongDTO, Integer> tcLP_DonGia;
-	
+	@FXML
+	TableView<NhanVienDTO> tvNhanVien;
+	@FXML
+	TableColumn<NhanVienDTO, Integer> tcNV_STT;
+	@FXML
+	TableColumn<NhanVienDTO, Integer> tcNV_MaNhanVien;
+	@FXML
+	TableColumn<NhanVienDTO, String> tcNV_TenNhanVien;
+	@FXML
+	TableColumn<NhanVienDTO, String> tcNV_CMND;
+	@FXML
+	TableColumn<NhanVienDTO, String> tcNV_SoDienThoai;
+	@FXML
+	TableColumn<NhanVienDTO, String> tcNV_DiaChi;
+	@FXML
+	TableColumn<NhanVienDTO, String> tcNV_Email;
+	@FXML
+	TableColumn<NhanVienDTO, String> tcNV_ChucVu;
+
+	@FXML
+	Label lbTS_SoNgayTraCoc;
+	@FXML
+	Label lbTS_TiLeThueVAT;
+	@FXML
+	Label lbTS_TiLeTienCoc;
+	@FXML
+	Label lbTS_QuaKhach;
+	@FXML
+	Label lbTS_TraPhongTre;
+
+	@FXML
+	TableView<LoaiDichVuDTO> tvLoaiDichVu;
+	@FXML
+	TableColumn<LoaiDichVuDTO, Integer> tcLDV_STT;
+	@FXML
+	TableColumn<LoaiDichVuDTO, Integer> tcLDV_MaLoaiDV;
+	@FXML
+	TableColumn<LoaiDichVuDTO, String> tcLDV_TenLoaiDV;
+
+	@FXML
+	TableView<NhaCungCapDTO> tvNhaCungCap;
+	@FXML
+	TableColumn<NhaCungCapDTO, Integer> tcNCC_STT;
+	@FXML
+	TableColumn<NhaCungCapDTO, Integer> tcNCC_MaNhaCC;
+	@FXML
+	TableColumn<NhaCungCapDTO, String> tcNCC_TenNhaCC;
+	@FXML
+	TableColumn<NhaCungCapDTO, Integer> tcNCC_SoDienThoai;
+
+	@FXML
+	TableView<LoaiPhongDTO> tvLoaiPhong;
+	@FXML
+	TableColumn<LoaiPhongDTO, Integer> tcLP_STT;
+	@FXML
+	TableColumn<LoaiPhongDTO, Integer> tcLP_MaLoaiPhong;
+	@FXML
+	TableColumn<LoaiPhongDTO, String> tcLP_TenLoaiPhong;
+	@FXML
+	TableColumn<LoaiPhongDTO, Integer> tcLP_SoKhachToiDa;
+	@FXML
+	TableColumn<LoaiPhongDTO, Integer> tcLP_DonGia;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		initTables();
 		loadTables();
 	}
 	
+	private boolean confirmDialog(String content) {
+		Alert dialog = new Alert(AlertType.CONFIRMATION);
+		dialog.setTitle("Xác nhận");
+		dialog.setHeaderText(content);
+		ButtonType yesButton = new ButtonType("Xác nhận");
+		ButtonType noButton = new ButtonType("Hủy bỏ");
+		dialog.getButtonTypes().setAll(yesButton, noButton);
+		Optional<ButtonType> result = dialog.showAndWait();
+		return result.get() == yesButton;
+	}
+
 	private void initTables() {
 		initTableNhanVien();
 		initTableLoaiPhong();
 		initTableNhaCungCap();
 		initTableLoaiDichVu();
 	}
-	
+
 	private void loadTables() {
 		loadTableNhanVien();
 		loadTableLoaiPhong();
@@ -70,19 +113,19 @@ public class MainController implements Initializable {
 		loadTableLoaiDichVu();
 		loadTableThamSo();
 	}
-	
+
 	private void initTableNhanVien() {
 		tcNV_STT.setCellValueFactory(
 				column -> new ReadOnlyObjectWrapper<Integer>(tvNhanVien.getItems().indexOf(column.getValue()) + 1));
 		tcNV_MaNhanVien.setCellValueFactory(new PropertyValueFactory<>("maNhanVien"));
 		tcNV_TenNhanVien.setCellValueFactory(new PropertyValueFactory<>("tenNhanVien"));
-		tcNV_CMND.setCellValueFactory(new PropertyValueFactory<>("CMND"));		
+		tcNV_CMND.setCellValueFactory(new PropertyValueFactory<>("CMND"));
 		tcNV_DiaChi.setCellValueFactory(new PropertyValueFactory<>("diaChi"));
 		tcNV_Email.setCellValueFactory(new PropertyValueFactory<>("email"));
-		tcNV_SoDienThoai.setCellValueFactory(new PropertyValueFactory<>("soDienThoai"));		
-		tcNV_ChucVu.setCellValueFactory(new PropertyValueFactory<>("chucVu"));		
+		tcNV_SoDienThoai.setCellValueFactory(new PropertyValueFactory<>("soDienThoai"));
+		tcNV_ChucVu.setCellValueFactory(new PropertyValueFactory<>("chucVu"));
 	}
-	
+
 	private void initTableLoaiPhong() {
 		tcLP_STT.setCellValueFactory(
 				column -> new ReadOnlyObjectWrapper<Integer>(tvLoaiPhong.getItems().indexOf(column.getValue()) + 1));
@@ -91,14 +134,14 @@ public class MainController implements Initializable {
 		tcLP_SoKhachToiDa.setCellValueFactory(new PropertyValueFactory<>("soKhachToiDa"));
 		tcLP_DonGia.setCellValueFactory(new PropertyValueFactory<>("donGia"));
 	}
-	
+
 	private void initTableLoaiDichVu() {
 		tcLDV_STT.setCellValueFactory(
 				column -> new ReadOnlyObjectWrapper<Integer>(tvLoaiDichVu.getItems().indexOf(column.getValue()) + 1));
 		tcLDV_MaLoaiDV.setCellValueFactory(new PropertyValueFactory<>("maLoaiDichVu"));
-		tcLDV_TenLoaiDV.setCellValueFactory(new PropertyValueFactory<>("tenLoaiDichVu"));		
+		tcLDV_TenLoaiDV.setCellValueFactory(new PropertyValueFactory<>("tenLoaiDichVu"));
 	}
-	
+
 	private void initTableNhaCungCap() {
 		tcNCC_STT.setCellValueFactory(
 				column -> new ReadOnlyObjectWrapper<Integer>(tvNhaCungCap.getItems().indexOf(column.getValue()) + 1));
@@ -114,8 +157,7 @@ public class MainController implements Initializable {
 				dsNhanVien.add(nv);
 			}
 			tvNhanVien.setItems(dsNhanVien);
-		}
-		catch (SQLException SQLException) {
+		} catch (SQLException SQLException) {
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Lỗi");
 			alert.setHeaderText("Không thể tải danh sách loại dịch vụ!");
@@ -123,7 +165,7 @@ public class MainController implements Initializable {
 			alert.showAndWait();
 		}
 	}
-	
+
 	public void loadTableLoaiPhong() {
 		try {
 			ObservableList<LoaiPhongDTO> dsLoaiPhong = FXCollections.observableArrayList();
@@ -131,8 +173,7 @@ public class MainController implements Initializable {
 				dsLoaiPhong.add(lp);
 			}
 			tvLoaiPhong.setItems(dsLoaiPhong);
-		}
-		catch (SQLException SQLException) {
+		} catch (SQLException SQLException) {
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Lỗi");
 			alert.setHeaderText("Không thể tải danh sách loại phòng!");
@@ -140,7 +181,7 @@ public class MainController implements Initializable {
 			alert.showAndWait();
 		}
 	}
-	
+
 	public void loadTableNhaCungCap() {
 		try {
 			ObservableList<NhaCungCapDTO> dsNhaCungCap = FXCollections.observableArrayList();
@@ -148,16 +189,15 @@ public class MainController implements Initializable {
 				dsNhaCungCap.add(ncc);
 			}
 			tvNhaCungCap.setItems(dsNhaCungCap);
-		}
-		catch (SQLException SQLException) {
+		} catch (SQLException SQLException) {
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Lỗi");
 			alert.setHeaderText("Không thể tải danh sách nhà cung cấp!");
 			alert.setContentText("Lỗi database!");
 			alert.showAndWait();
-		}		
+		}
 	}
-	
+
 	public void loadTableLoaiDichVu() {
 		try {
 			ObservableList<LoaiDichVuDTO> dsLoaiDichVu = FXCollections.observableArrayList();
@@ -165,8 +205,7 @@ public class MainController implements Initializable {
 				dsLoaiDichVu.add(ldv);
 			}
 			tvLoaiDichVu.setItems(dsLoaiDichVu);
-		}
-		catch (SQLException SQLException) {
+		} catch (SQLException SQLException) {
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Lỗi");
 			alert.setHeaderText("Không thể tải danh sách loại dịch vụ!");
@@ -174,7 +213,7 @@ public class MainController implements Initializable {
 			alert.showAndWait();
 		}
 	}
-	
+
 	public void loadTableThamSo() {
 		try {
 			ThamSoDTO thamSo = ThamSoBUS.getThamSo();
@@ -183,13 +222,49 @@ public class MainController implements Initializable {
 			lbTS_TiLeThueVAT.setText(String.format("%.0f", thamSo.getTiLeThueVAT() * 100) + "%");
 			lbTS_QuaKhach.setText(String.format("%.0f", thamSo.getPhuThuQuaKhach() * 100) + "%");
 			lbTS_TraPhongTre.setText(String.format("%.0f", thamSo.getPhuthuTraPhongTre() * 100) + "%");
-		}
-		catch (SQLException SQLException) {
+		} catch (SQLException SQLException) {
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Lỗi");
 			alert.setHeaderText("Không thể tải danh sách tham số!");
 			alert.setContentText("Lỗi database!");
-			alert.showAndWait();			
+			alert.showAndWait();
+		}
+	}
+
+	// Xóa loại phòng
+	public void handleXoaLoaiPhong(ActionEvent e) {
+		try {
+			LoaiPhongDTO loaiPhong = tvLoaiPhong.getSelectionModel().getSelectedItem();
+			if (confirmDialog("Xác nhận xóa loại phòng " + loaiPhong.getTenLoaiPhong() + "?")) {
+				try {
+					if (LoaiPhongBUS.deleteLoaiPhong(loaiPhong.getMaLoaiPhong())) {
+						Alert alert = new Alert(AlertType.INFORMATION);
+						alert.setTitle("Thành công");
+						alert.setHeaderText("Xóa loại phòng thành công!");
+						alert.setContentText("Đã xóa loại phòng " + loaiPhong.getTenLoaiPhong() + "!");
+						alert.showAndWait();
+						loadTableLoaiPhong();
+					} else {
+						Alert alert = new Alert(AlertType.INFORMATION);
+						alert.setTitle("Lỗi");
+						alert.setHeaderText("Không thể xóa loại phòng!");
+						alert.setContentText("Vẫn còn phòng loại " + loaiPhong.getTenLoaiPhong() + "!");
+						alert.showAndWait();
+					}
+				} catch (SQLException SQLException) {
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Lỗi");
+					alert.setHeaderText("Không thể xóa loại phòng!");
+					alert.setContentText("Lỗi database!");
+					alert.showAndWait();
+				}
+			}
+		} catch (NullPointerException NullPointerException) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Lỗi");
+			alert.setHeaderText("Không thể xóa loại phòng!");
+			alert.setContentText("Vui lòng chọn loại phòng cần xóa!");
+			alert.showAndWait();
 		}
 	}
 }
